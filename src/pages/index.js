f321ae7e97286d65350c6966da3132ca1abefddc
tramanhvong <vongta21@my.yorku.ca>
2024-02-17 12:01:36 -0500

    weather api fetching information

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,8 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import axios from 'axios'
 import {useState} from 'react'
-import {BsSearch} from 'react-icons/bs'
+import {BsSearch} from 'react-icons/bs';
+import Weather from '../components/weather'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,17 +14,18 @@ export default function Home() {
   const [weater, setWeather] = useState({});
   const [loading, setLoading] = useState(false);
 
-  const url = 'https://api.openweathermap.org/data/2.5/weather?q=toronto&appid={process.env.hUH}'
 
   const fetchWeather = (e) => {
-    e.preventDefault()
-    setLoading(true)
+    e.preventDefault();
+    setLoading(true);
+    
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=77fcc8ce22ab9dede2f8ce43331f6dcf`
     axios.get(url).then((response) => {
       setWeather(response.data)
       console.log(response.data)
     })
-    setCity('')
-    setLoading(false)
+    setCity('');
+    setLoading(false);
   }
   return (
     <>
@@ -33,102 +35,31 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <button onClick={fetchWeather}>Fetch data</button>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      
+      <div>
+        {/* <form onSubmit={fetchWeather} className='zipCodeForm'>
+            <div>
+                <input onChange={(e) => setZipCode(e.target.value)}
+                    className='zipCodeForm'
+                    type = 'text'
+                    placeholder='Type zipcode'
+                />
+            </div>
+        </form> */}
+        <form onSubmit={fetchWeather} className='cityForm'>
+            <div>
+                <input onChange={(e) => setCity(e.target.value)}
+                    className='cityForm'
+                    type = 'text'
+                    placeholder='Search city'
+                />
+            </div>
+        </form>
+        <button onClick={fetchWeather}>Fetch data 
+                <BsSearch size={20}/>
+        </button>
+        {Weather.main && <Weather data={weather}/>}
+      </div>
     </>
   );
 }
