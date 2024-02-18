af15f585321554037ff5c47a227e247d266d6bb7
claire <github@cval.dev>
2024-02-18 00:48:34 -0500

    send request btn

--- src/pages/clients/index.js
+++ src/pages/clients/index.js
@@ -1,35 +1,52 @@
-import { Alert, Grid } from '@mui/material';
-import { columns, options } from '@/ref/clientTable';
-import TableData from '@/components/Table/TableData';
-import { data } from '@/data/helper';
+import { Alert, Grid, Button, Container, Stack } from "@mui/material";
+import { columns, options } from "@/ref/clientTable";
+import TableData from "@/components/Table/TableData";
+import { data } from "@/data/helper";
+import { useState } from "react";
+import { SuccessSnackBar } from "../components/SuccessSnackBar";
 
 export default function Client() {
-  return (
-    <Grid container spacing={2} paddingTop={8} paddingLeft={5}>
-      <Grid item xs={8}>
-        <Grid container direction='column' spacing={2}>
-          <Grid item>
-            <Alert variant='filled' severity='success' style={{ width: '40%' }}>
-              We found 1 volunteer for you!
-            </Alert>
-          </Grid>
-          <Grid item>
-            {data.length === 0 && (
-              <Alert
-                variant='filled'
-                severity='warning'
-                style={{ width: '40%' }}
-              >
-                No volunteers found
-              </Alert>
-            )}
-            <TableData data={data} columns={columns} options={options} />
-          </Grid>
-        </Grid>
-      </Grid>
-      <Grid item xs={4}>
-        <div>Details</div>
-      </Grid>
-    </Grid>
-  );
+    const [requestSent, setRequestSent] = useState(false);
+    const handleOnClickRequest = async (e) => {
+        e.preventDefault();
+        const res = await fetch(
+            `${process.env.NEXT_PUBLIC_HOST}/api/insertRequest`
+        );
+
+        if (res.ok) {
+            setRequestSent(true);
+        }
+    };
+    return (
+        <Container>
+            <Stack paddingTop={2} spacing={2}>
+                <Grid item>
+                    <Button variant="contained" onClick={handleOnClickRequest}>
+                        Send Request
+                    </Button>
+                </Grid>
+                <Grid item>
+                    {data.length === 0 && (
+                        <Alert
+                            variant="filled"
+                            severity="warning"
+                            style={{ width: "40%" }}
+                        >
+                            No volunteers found
+                        </Alert>
+                    )}
+                    <TableData
+                        data={data}
+                        columns={columns}
+                        options={options}
+                    />
+                </Grid>
+            </Stack>
+            <SuccessSnackBar
+                open={requestSent}
+                onClose={() => setRequestSent(false)}
+                message="Request Sent"
+            />
+        </Container>
+    );
 }
---
+++ src/pages/components/SuccessSnackBar.js
@@ -0,0 +1,19 @@
+import { Snackbar, Alert, Typography } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+
+export function SuccessSnackBar({ open, onClose, message }) {
+    return (
+        <Snackbar
+            open={open}
+            autoHideDuration={6000}
+            onClose={onClose}
+            anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        >
+            <Alert icon={<CheckIcon fontSize="large" />} severity="success">
+                <Typography fontSize="2em" fontWeight="bold">
+                    {message ?? "Success!"}
+                </Typography>
+            </Alert>
+        </Snackbar>
+    );
+}
